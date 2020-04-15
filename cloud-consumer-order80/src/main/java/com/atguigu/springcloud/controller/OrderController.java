cfc6fa60f39e6303ed4cf3de7c887af5880a536a
“huao2416” <huao2416@163.com>
2020-04-15 22:40:28 +0800

    #[huao] 首次忽略文件

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -1,24 +1,19 @@
 package com.atguigu.springcloud.controller;
 
-import cn.hutool.json.JSON;
-import com.atguigu.springcloud.bean.SyncVehicleRequest;
-import com.atguigu.springcloud.bean.TestReq;
-import com.atguigu.springcloud.bean.TestRes;
-import com.atguigu.springcloud.bean.VmsVehicleMsg;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import springcloud.entities.CommonResult;
 import springcloud.entities.Payment;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @Author: Hugh.Hu
  * @Date: 2020/4/6 11:54 上午
  */
 @RestController
+@Slf4j
 public class OrderController
 {
     public static final String PAYMENT_URL = "http://localhost:8001";
@@ -34,12 +29,6 @@ public class OrderController
     @GetMapping(value = "/consumer/payment/get/{id}")
     public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
         return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
-    }
-
-    @PostMapping(value = "/fota/getCarMessageList")
-    public TestRes getCarMessageList(@RequestBody  TestReq req){
-        return restTemplate.postForObject("http://221.192.228.244:28001/gwmNEWVMSServer/fota/getCarMessageList",req,
-                TestRes.class);
     }
 
     }
